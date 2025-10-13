8d12533afe0d117cfd468751bd869c18041fcaa7
TimurManjosov <cloud-timur.16@outlook.de>
2025-10-13 18:02:28 +0200

    feat(intl): add number, percent, currency, and date filters

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,6 +1,6 @@
 import type { Filter } from './text';
 import { lower, plural, trim, upper } from './text';
-
+export { makeIntlFilters } from './intl';
 export type { Filter } from './text';
 
 export const builtinFilters: Record<string, Filter> = {
